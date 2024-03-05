4fc9da6f56630be79893ac2fa3680288ff47111b
Hieu05 <hieuhip.05052004gmail.com>
2024-03-05 11:08:07 +0700

    project

---
+++ config.js
@@ -0,0 +1,12 @@
+const nameGirl = 'Bé iu';
+const giftUrl = 'https://www.facebook.com/huynhkyngan214/';
+const eventName = 'Chúc Mừng 08-03';
+const titleCard = 'Tặng Bé iu Của chị';
+const contentCard = 'Chúc embe 8/3 duii dẻ nhooo. Cảm ơn em vì đã ở bên chị. Hy vọng là chúng ta sẽ mãi luôn như vậy. Chị thương bé';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Hiếu https://www.facebook.com/HieuFF.05/
+const giftImage = 'ngan.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
